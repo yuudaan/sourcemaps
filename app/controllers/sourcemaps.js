9998af37a5adba07b819300cdc7adbadda97f9e3
yudan <yudan@gaoding.com>
2019-09-25 15:07:39 +0800

    fix: 禁止浏览器加载 sourcemaps

--- app/controllers/sourcemaps.js
+++ app/controllers/sourcemaps.js
@@ -1,4 +1,8 @@
 exports.verifyHeader = ctx => {
+    if (ctx.headers['user-agent']) {
+        ctx.status = 404;
+        return;
+    }
     ctx.status = 302;
     ctx.redirect(`http://cdn.dancf.com/odyssey-editor${ctx.url}`);
 };
