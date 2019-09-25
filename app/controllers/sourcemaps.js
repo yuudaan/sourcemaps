6458f813f6dbe346b04fc708d74606a71cdb6553
yudan <yudan@gaoding.com>
2019-09-25 15:49:31 +0800

    fix: 按 IP 限制访问

--- app/controllers/sourcemaps.js
+++ app/controllers/sourcemaps.js
@@ -1,8 +1,17 @@
+function allowed(ip) {
+    const WhiteList = [
+        '218.17.158.233', // 深圳 office
+        '218.104.142.203', // 厦门 office
+        '47.99.133.85',  // sentry.hlgdata.com
+    ];
+    return WhiteList.indexOf(ip) > -1;
+}
+
 exports.verifyHeader = ctx => {
-    if (ctx.headers['user-agent']) {
+    if (allowed(ctx.headers['x-real-ip']) || allowed(ctx.headers['x-forwarded-for'])) {
+        ctx.status = 302;
+        ctx.redirect(`http://cdn.dancf.com/odyssey-editor${ctx.url}`);
+    } else {
         ctx.status = 404;
-        return;
     }
-    ctx.status = 302;
-    ctx.redirect(`http://cdn.dancf.com/odyssey-editor${ctx.url}`);
 };
