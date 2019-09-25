e4cfefa51fb868c2d9bfbf3fe040e0895e234557
yudan <yudan@gaoding.com>
2019-09-25 11:57:06 +0800

    fix: sentry 没有发 header

--- app/controllers/sourcemaps.js
+++ app/controllers/sourcemaps.js
@@ -1,6 +1,5 @@
-const SentryHeader = 'x-sentry-token';
 exports.verifyHeader = ctx => {
-    if (ctx.headers[SentryHeader] === 'a668d1a891a911e98d19965914fd28fc') {
+    if (ctx.headers['x-real-ip'] === '218.17.158.233') {
         ctx.status = 302;
         ctx.redirect(`http://cdn.dancf.com/odyssey-editor/sourcemaps${ctx.url}`);
     }
