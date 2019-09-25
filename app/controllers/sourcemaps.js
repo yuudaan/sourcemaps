1f17c2125caf031f66d4d4fae33ca7e72f275225
yudan <yudan@gaoding.com>
2019-09-25 14:03:24 +0800

    fix

--- app/controllers/sourcemaps.js
+++ app/controllers/sourcemaps.js
@@ -1,6 +1,4 @@
 exports.verifyHeader = ctx => {
-    if (ctx.headers['x-real-ip'] === '218.17.158.233') {
-        ctx.status = 302;
-        ctx.redirect(`http://cdn.dancf.com/odyssey-editor/sourcemaps${ctx.url}`);
-    }
+    ctx.status = 302;
+    ctx.redirect(`http://cdn.dancf.com/odyssey-editor${ctx.url}`);
 };
